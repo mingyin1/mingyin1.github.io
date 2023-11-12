cf9589062ad907e15b5eb810dacf89fcc483eb19
mingyin1 <ym2003106666@gmail.com>
2023-11-12 22:27:22 +0800

    new

--- libs/custom/my_js.js
+++ libs/custom/my_js.js
@@ -33,7 +33,7 @@ $(document).ready(function() {
         menu = target;
     $target = $(target);
     $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-20
+        'scrollTop': $target.offset().top
     }, 0, 'swing', function () {
         window.location.hash = target;
         $(document).on("scroll", onScroll);
